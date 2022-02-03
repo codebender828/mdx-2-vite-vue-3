8952fa4bd43bfc8d511c1021321c813c788ee6b6
carwack <sybren.willemot@gmail.com>
2022-02-03 16:40:23 +0100

    feat(*): add remark plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import mdx from '@mdx-js/rollup'
 import { babel } from '@rollup/plugin-babel'
 
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkGfm from "remark-gfm"
+import remarkAutolinkHeadings from "remark-autolink-headings"
+import remarkSlug from "remark-slug"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     mdx({
       jsx: true,
+      remarkPlugins: [
+        remarkFrontmatter,
+        remarkGfm,
+        remarkAutolinkHeadings,
+        remarkSlug
+      ]
     }),
     babel({
       // Also run on what used to be `.mdx` (but is now JS):
